e16a521a3beea8afc90b1c3ec58c26c2b8b0eec4
Sree Dharshni <sreedharshniilango@gmail.com>
2024-10-26 00:29:18 +0530

    second  commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose
+  .connect("mongodb://localhost:27017/mydatabase")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(config.PORT, () => {
+      console.log(`Server running on ${config.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(config.URL)
+    console.error("Error Connecting to MongoDB", error);
+  });
